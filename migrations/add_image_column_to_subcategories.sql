06f246f73ecfc3858ca7f5e21ccbb713c12df0e1
TcKatFire <matvei201357@gmail.com>
2026-06-10 14:29:47 +0700

    Remove unnecessary migration - subcategories don't need images

--- migrations/add_image_column_to_subcategories.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Add image column to subcategories table
--- Date: 2026-06-10
--- Description: Adds image field to subcategories to allow setting subcategory images
-
-ALTER TABLE subcategories ADD COLUMN IF NOT EXISTS image TEXT;
-
-COMMENT ON COLUMN subcategories.image IS 'URL изображения подкатегории';
